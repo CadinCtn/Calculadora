b499dc2f7aaf6eea0eff336dbea5c6d8fae2b6d6
Cristyan Gabriel Rocha <cristyanrochaoff@gmail.com>
2023-12-04 16:51:30 -0300

    Calculadora
    Gera valores e uma operação aleatória, o usuario digita o resultado.

---
+++ calculadoraAleatoria/src/calculadoraaleatoria/CalculadoraAleatoria.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadoraaleatoria;
+
+import gui.Calculadora;
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CalculadoraAleatoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Calculadora calculadora = new Calculadora();
+        calculadora.setVisible(true);
+        calculadora.setLocationRelativeTo(null);
+        
+        
+        
+        
+    }
+    
+}
